f50a8dfb6b26cc33d0017d118a098f5fe3ef5a8d
Aruj Bansal <bansalaruj77@gmail.com>
2023-06-07 03:10:06 +0530

    Login Page

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import "package:flutter/material.dart";
+
+class Constants {
+  static String appId= "1:890213074063:web:be51484963d2f3e5859cfa";
+  static String apiKey= "AIzaSyBL9EEL0Ymmt4fpkYae9S6Q2xwbRaQthLk";
+  static String messagingSenderId= "890213074063";
+  static String projectId= "chat-app-ba39c";
+  final primaryColor=const Color.fromARGB(255, 57, 13, 160);
+}
---
+++ lib/widgets/widget.dart
@@ -0,0 +1,13 @@
+import "package:flutter/material.dart";
+const textInputDecoration=InputDecoration(
+  labelStyle: TextStyle(color: Colors.black, fontWeight: FontWeight.w300),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Color.fromARGB(255, 57, 13, 160))
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Color.fromARGB(255, 57, 13, 160))
+  ),
+  errorBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Color.fromARGB(255, 57, 13, 160))
+  ),
+);
